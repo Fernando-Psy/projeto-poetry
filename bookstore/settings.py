f8ac754b18739965bfe19aadc047fd9e3b4814de
Fernando-Psy <fcesarjunior@gmail.com>
2025-05-27 08:57:20 -0300

    Ajustando a atividade

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -33,9 +33,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+    #"DEFAULT_PERMISSION_CLASSES": [
+    #   "rest_framework.permissions.IsAuthenticated",
+    #],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
