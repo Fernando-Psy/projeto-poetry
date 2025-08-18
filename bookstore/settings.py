d5c22b14755c5e1866af2bd838c22a94e0eaa31e
Fernando-Psy <fcesarjunior@gmail.com>
2025-08-18 08:16:51 -0300

    Atualizando ALLOWED_HOSTS para incluir a URL de produção do Heroku

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure--8h&@*d11b#mthic!+!7hfls-=okpi(e!9d+o=7cb!v_hq-590
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://bookstore-fcbj-api-dd2558886ea1.herokuapp.com/"]
 
 
 REST_FRAMEWORK = {
