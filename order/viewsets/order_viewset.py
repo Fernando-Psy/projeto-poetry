6a9bc4b5686f107b0523058210ce2c406fd246e4
Fernando Cesar B. Junior <113809912+Fernando-Psy@users.noreply.github.com>
2025-10-27 11:51:42 -0300

    Fix typo in permission class for OrderViewSet

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,13 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated as IsAutenticated
+from rest_framework.permissions import IsAuthenticated
 from order.models import Order
 from order.serializers.order_serializer import OrderSerializer
-
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.prefetch_related("items__product").all()
     serializer_class = OrderSerializer
-
     authentication_classes = [BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAutenticated]
+    permission_classes = [IsAuthenticated]
