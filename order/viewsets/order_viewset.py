f505bc585d75685aa8e4e36b2ac072d65eca0ead
Fernando Cesar <fcesarjunior@gmail.com>
2025-11-16 11:37:50 -0300

    API pública para avaliação1

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,11 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from order.models import Order
 from order.serializers.order_serializer import OrderSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.prefetch_related("items__product").all()
     serializer_class = OrderSerializer
-    authentication_classes = [BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
